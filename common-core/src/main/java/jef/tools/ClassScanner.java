8aff738f8870afc8e0f635a069e0483b2e6b5942
Joey <mr.jiyi@gmail.com>
2017-07-26 19:13:58 +0800

    1.修复包扫描插件的问题（Since 1.11.7）
    2.针对多EMF下 Bean区分进行改进。

--- common-core/src/main/java/jef/tools/ClassScanner.java
+++ common-core/src/main/java/jef/tools/ClassScanner.java
@@ -76,6 +76,7 @@ public class ClassScanner {
 			}
 			result.addAll(Arrays.asList(res));
 		}
+		IOUtils.closeQuietly(cl);
 		return result.toArray(new IResource[result.size()]);
 	}
 
--- jef-maven-plugin/src/main/java/org/jef/mavenplugin/goal/EnhanceMojo.java
+++ jef-maven-plugin/src/main/java/org/jef/mavenplugin/goal/EnhanceMojo.java
@@ -1,6 +1,8 @@
 package org.jef.mavenplugin.goal;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 import jef.codegen.EntityEnhancer;
 import jef.common.log.LogUtil;
@@ -15,80 +17,77 @@ import org.apache.maven.plugin.MojoExecutionException;
  * @phase process-classes
  */
 public class EnhanceMojo extends AbstractMojo {
-	/**
-	 * @parameter expression="${enhance.path}"
-	 */
-	private String path;
+    /**
+     * @parameter expression="${enhance.path}"
+     */
+    private String path;
 
-	/**
-	 * The directory containing generated classes.
-	 * 
-	 * @parameter expression="${project.build.outputDirectory}"
-	 * @required
-	 * 
-	 */
-	private File classesDirectory;
+    /**
+     * The directory containing generated classes.
+     * 
+     * @parameter expression="${project.build.outputDirectory}"
+     * @required
+     * 
+     */
+    private File classesDirectory;
 
-	/**
-	 * 基路径
-	 * 
-	 * @parameter expression="${basedir}"
-	 * @required
-	 * @readonly
-	 */
-	protected String basedir;
-	
-	public void setPath(String path) {
-		this.path = path;
-	}
+    /**
+     * 基路径
+     * 
+     * @parameter expression="${basedir}"
+     * @required
+     * @readonly
+     */
+    protected String basedir;
 
-	public void execute() throws MojoExecutionException {
-		long time = System.currentTimeMillis();
-		try {
-			this.getLog().info("Easybuilder enhanceing entity classes......");
+    public void setPath(String path) {
+        this.path = path;
+    }
 
-			String workPath;
-			if (path == null)
-				workPath = classesDirectory.getAbsolutePath();
-			else
-				workPath = path;
-			
-		
-			
-			workPath = workPath.replace('\\', '/');
-			this.getLog().info("Easybuilder enhance entity classes working path is: " + workPath);
+    public void execute() throws MojoExecutionException {
+        long time = System.currentTimeMillis();
+        try {
+            this.getLog().info("Easybuilder enhanceing entity classes......");
 
-			EntityEnhancer en = new EntityEnhancer();
-			en.enhance();
+            String workPath;
+            if (path == null) {
+                workPath = classesDirectory.getAbsolutePath();
+            } else {
+                workPath = path;
+            }
+            workPath = workPath.replace('\\', '/');
+            if (!workPath.endsWith("/")) {
+                workPath = workPath + "/";
+            }
+            this.getLog().info("Easybuilder enhance entity classes working path is: " + workPath);
 
-			this.getLog().info("Easybuilder enhance entity classes total use " + (System.currentTimeMillis() - time) + "ms");
-		} catch (Exception e) {
-			this.getLog().error(e);
-		}
-	}
+            EntityEnhancer en = new EntityEnhancer().addRoot(new URL("file://" + workPath));
+            en.enhance();
 
-	//TEST
-	public static void main(String[] args) {
-		String workPath = "D:/account-receivable-1.0.8-20111214.085530-31-api";
-		try {
-			workPath = workPath.replace('\\', '/');
-			EntityEnhancer en = new EntityEnhancer();
-			en.enhance();
-		} catch (Exception e) {
-			LogUtil.error(e);
-		}
-	}
+            this.getLog().info("Easybuilder enhance entity classes total use " + (System.currentTimeMillis() - time) + "ms");
+        } catch (Exception e) {
+            this.getLog().error(e);
+        }
+    }
 
-	public String getPath() {
-		return path;
-	}
+    // TEST
+    public static void main(String[] args) throws IOException, MojoExecutionException {
+        String workPath = "E:/Git/ef-orm/orm-test/target/test-classes";
+        EnhanceMojo em=new EnhanceMojo();
+        em.setPath(workPath);
+        em.execute();
+    }
 
-	public File getClassesDirectory() {
-		return classesDirectory;
-	}
-	
-	public String getBasedir() {
-		return basedir;
-	}
+    public String getPath() {
+        return path;
+    }
+
+    public File getClassesDirectory() {
+        return classesDirectory;
+    }
+
+    public String getBasedir() {
+        return basedir;
+    }
 
 }
