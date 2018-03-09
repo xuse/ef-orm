bec98612e9c191824bc84016415c8164ffe1ae74
Joey <mr.jiyi@gmail.com>
2018-03-09 11:14:38 +0800

    一种特殊的情况下，并发错误问题修复。

--- common-orm/src/main/java/jef/database/innerpool/PoolReleaseThread.java
+++ common-orm/src/main/java/jef/database/innerpool/PoolReleaseThread.java
@@ -10,83 +10,79 @@ import jef.tools.ThreadUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * 全局唯一的池收缩器.每个
+ * 
  * @author jiyi
  *
  */
-public class PoolReleaseThread extends Thread{
-	private static PoolReleaseThread prt=new PoolReleaseThread();
-	
-	private Logger log=LoggerFactory.getLogger(this.getClass());
-	private boolean alive=true;
-	private final ConcurrentLinkedQueue<IPool<?>> pools=new ConcurrentLinkedQueue<IPool<?>>();
-	
-	
-	
+public class PoolReleaseThread extends Thread {
+	private static PoolReleaseThread prt = new PoolReleaseThread();
+
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	private boolean alive = true;
+	private final ConcurrentLinkedQueue<IPool<?>> pools = new ConcurrentLinkedQueue<IPool<?>>();
+
 	private PoolReleaseThread() {
 		super();
 		setDaemon(true);
 	}
 
-	public void addPool(IPool<?> ip){
+	public void addPool(IPool<?> ip) {
 		pools.add(ip);
-		if(!isAlive() && alive){
-			start();	
+		synchronized (this) {
+			if (!isAlive() && alive) {
+				start();
+			}
 		}
 	}
-	
-	public void removePool(IPool<?> ip){
+
+	public void removePool(IPool<?> ip) {
 		pools.remove(ip);
-		
+
 	}
-	
-	public static PoolReleaseThread getInstance(){
+
+	public static PoolReleaseThread getInstance() {
 		return prt;
 	}
 
-	public void close(){
-		this.alive=false;
+	public void close() {
+		this.alive = false;
 	}
-	
+
 	@Override
 	public void run() {
-		long sleep=JefConfiguration.getLong(DbCfg.DB_CONNECTION_LIVE,60000);
+		long sleep = JefConfiguration.getLong(DbCfg.DB_CONNECTION_LIVE, 60000);
 		ThreadUtils.doSleep(sleep);
-		try{
-			while(alive){
-				doWork();
+		try {
+			while (alive) {
+				for (IPool<?> pool : pools) {
+					try {
+						pool.closeConnectionTillMin();
+					} catch (Exception e) {
+						log.error("release connecton pool error", e);
+					}
+				}
 				ThreadUtils.doSleep(sleep);
 			}
-		}catch(Exception e){
-			log.error("",e);
+		} catch (Exception e) {
+			log.error("", e);
 		}
 	}
 
-	private void doWork() {
-		for(IPool<?> pool:pools){
-			try{
-				pool.closeConnectionTillMin();
-			}catch(Exception e){
-				log.error("release connecton pool error",e);
-			}
-		}
-	}
-	
 	/**
 	 * this function is using for debug the connection pool.
 	 * 
 	 * it will check ths poll count and offer count of pools.
 	 */
-	public void assertPoolCount(){
-		for(IPool<?> pool:pools){
-			PoolStatus st=pool.getStatus();
-			if(st.getPollCount()!=st.getOfferCount()){
+	public void assertPoolCount() {
+		for (IPool<?> pool : pools) {
+			PoolStatus st = pool.getStatus();
+			if (st.getPollCount() != st.getOfferCount()) {
 				PoolService.logPoolStatic(pool.getClass().getSimpleName(), st.getPollCount(), st.getOfferCount());
 				throw new IllegalStateException(st.toString());
 			}
-			
+
 		}
 	}
 }
