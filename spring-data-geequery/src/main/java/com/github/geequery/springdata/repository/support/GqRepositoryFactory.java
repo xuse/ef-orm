f40428e850703a703ab79c380a44c7b384c4594f
xuse <a123456>
2019-04-06 14:06:07 +0800

    升级到Spring-data v2.1.5。更改Spring-data变更的API。

--- spring-data-geequery/src/main/java/com/github/geequery/springdata/repository/support/GqRepositoryFactory.java
+++ spring-data-geequery/src/main/java/com/github/geequery/springdata/repository/support/GqRepositoryFactory.java
@@ -20,19 +20,19 @@ import java.util.Optional;
 
 import javax.persistence.EntityManager;
 
-import jef.database.jpa.JefEntityManagerFactory;
-
 import org.springframework.data.repository.core.EntityInformation;
 import org.springframework.data.repository.core.RepositoryInformation;
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
-import org.springframework.data.repository.query.EvaluationContextProvider;
 import org.springframework.data.repository.query.QueryLookupStrategy;
 import org.springframework.data.repository.query.QueryLookupStrategy.Key;
+import org.springframework.data.repository.query.QueryMethodEvaluationContextProvider;
 import org.springframework.util.Assert;
 
 import com.github.geequery.springdata.repository.GqRepository;
 import com.github.geequery.springdata.repository.query.GqQueryLookupStrategy;
+
+import jef.database.jpa.JefEntityManagerFactory;
 
 /**
  * GeeQuery specific generic repository factory.
@@ -113,17 +113,9 @@ public class GqRepositoryFactory extends RepositoryFactorySupport {
 		return GqRepositoryImpl.class;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.springframework.data.repository.core.support.RepositoryFactorySupport
-	 * #getQueryLookupStrategy(org.springframework.data.repository.query.
-	 * QueryLookupStrategy.Key,
-	 * org.springframework.data.repository.query.EvaluationContextProvider)
-	 */
+
 	@Override
-	protected Optional<QueryLookupStrategy> getQueryLookupStrategy(Key key, EvaluationContextProvider evaluationContextProvider) {
+	protected Optional<QueryLookupStrategy> getQueryLookupStrategy(Key key, QueryMethodEvaluationContextProvider evaluationContextProvider) {
 		return Optional.<QueryLookupStrategy> of(new GqQueryLookupStrategy(emf));
 	}
 
